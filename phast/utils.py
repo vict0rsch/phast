920f33e17c65c62e82e4cddc0043c16154f304dc
Victor Schmidt <vsch@pm.me>
2023-02-28 19:11:36 -0500

    add `get_pbc_distances`

--- phast/utils.py
+++ phast/utils.py
@@ -5,6 +5,8 @@ try:
 except ImportError:
     PYG_OK = False
 
+import torch
+
 
 def ensure_pyg_ok(func):
     """
@@ -25,3 +27,48 @@ def ensure_pyg_ok(func):
         return func(*args, **kwargs)
 
     return wrapper
+
+
+def get_pbc_distances(
+    pos,
+    edge_index,
+    cell,
+    cell_offsets,
+    neighbors,
+    return_offsets=False,
+    return_distance_vec=False,
+):
+    """Compute distances between atoms with periodic boundary conditions.
+
+    From `OCP <https://github.com/Open-Catalyst-Project/ocp>`_.
+    """
+    row, col = edge_index
+
+    distance_vectors = pos[row] - pos[col]
+
+    # correct for pbc
+    neighbors = neighbors.to(cell.device)
+    cell = torch.repeat_interleave(cell, neighbors, dim=0)
+    offsets = cell_offsets.float().view(-1, 1, 3).bmm(cell.float()).view(-1, 3)
+    distance_vectors += offsets
+
+    # compute distances
+    distances = distance_vectors.norm(dim=-1)
+
+    # redundancy: remove zero distances
+    nonzero_idx = torch.arange(len(distances), device=distances.device)[distances != 0]
+    edge_index = edge_index[:, nonzero_idx]
+    distances = distances[nonzero_idx]
+
+    out = {
+        "edge_index": edge_index,
+        "distances": distances,
+    }
+
+    if return_distance_vec:
+        out["distance_vec"] = distance_vectors[nonzero_idx]
+
+    if return_offsets:
+        out["offsets"] = offsets[nonzero_idx]
+
+    return out
