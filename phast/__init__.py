12d7650318e98fb36f88645ec3488d7d265515ea
vict0rsch <vsch@pm.me>
2023-04-01 12:44:32 -0400

    update module docs

---
+++ phast/__init__.py
@@ -0,0 +1,10 @@
+"""
+``phast`` Python package structure:
+
+* ``phast.embedding`` ➡️ Physics-based embedding of atomic graphs,
+    notably :class:`phast.embedding.PhysEmbedding`
+* ``phast.graph_rewiring`` ➡️ OC20-specific graph rewiring functions, notably
+    :func:`phast.graph_rewiring.remove_tag0_nodes`
+
+To use ``phast.graph_rewiring``, you must install ```torch_geometric`` <https://pytorch-geometric.readthedocs.io/en/latest/install/installation.html>`_.
+"""
